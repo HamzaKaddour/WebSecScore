0dfd50461d242c0a05638c5ee8f42f12920a42c1
HK <hamzakaddour@isu.edu>
2024-10-17 14:11:20 -0600

    score fct

---
+++ Score/scoring.py
@@ -0,0 +1,10 @@
+from WebSecScore.Tests import hibp as HIBP
+from WebSecScore.Tests import malware_detect as MW
+from WebSecScore.Tests import open_ports as OP
+from WebSecScore.Tests import threatfox as COMPROMIZE
+from WebSecScore.Tests import virusTotal as VT
+from WebSecScore.Tests import vul_wapiti as VUL
+from WebSecScore.Tests import whatcms as CMS
+# from WebSecScore.Tests import 
+import requests
+
