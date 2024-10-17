a4e6d1a7df4bf45b9eed17ea870bc0ba6239ed8b
HK <hamzakaddour@isu.edu>
2024-10-16 18:48:17 -0600

    threatfox

---
+++ Tests/threatfox.py
@@ -0,0 +1,51 @@
+# Compromised IP or not
+
+import requests
+# import Services.utilities
+from Services import utilities
+
+def checkCompromisedURL(domain):
+    result = {
+        "isCompromised" : 0,
+        "info" : None
+    }
+    
+    try:
+        ip_addr = utilities.hostname_to_ip(domain)
+    except Exception as e:
+        print(e)
+        return result
+        
+    
+    url = "https://threatfox-api.abuse.ch/api/v1/"
+    data = {
+    "query": "search_ioc",
+    # "search_term": ip_addr
+    "search_term": "139.180.203.104"
+    }
+
+    
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            response_json = response.json()
+            # print(response_json)
+            if response_json["query_status"] == "no_result":
+                return result
+            elif response_json["query_status"] == "ok":
+                result["isCompromised"] = 1
+                result["info"] = response_json["data"]
+            else:
+                result["info"] = "This is not a valid URL"
+        else:
+            result["info"] = "invalid response code"
+    except Exception as e:
+        print(e)
+    finally:
+        return result
+            
+                	
+# print(checkCompromisedURL("http://sskymedia.com/VMYB-ht_JAQo-gi/INV/99401FORPO/20673114777/US/Outstanding-Invoices/"))
+
+# print(utilities.hostname_to_ip("http://sskymedia.com/VMYB-ht_JAQo-gi/INV/99401FORPO/20673114777/US/Outstanding-Invoices/"))
+print(checkCompromisedURL("google.com"))
